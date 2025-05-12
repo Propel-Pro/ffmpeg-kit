37be966ec047ab5969b96cf93d9d533585e2acad
Lawrence Spector <lawrence.spector@gmail.com>
2025-05-12 12:45:11 -0400

    Build script cleanup plus add a --help page

--- build.sh
+++ build.sh
@@ -3,16 +3,33 @@
 export ANDROID_SDK_ROOT=$HOME/android-r35.0.2
 export ANDROID_NDK_ROOT=$HOME/android-ndk-r26d
 
-if [[ $# -lt 1 ]]
-then
+show_help()
+{
     echo "Usage: $0 platform ..args.."
     echo "\tplatform can be one of: Android, iOS"
+}
+
+if [[ $# -lt 1 ]]
+then
+    show_help
+    exit -1
+fi
+
+if [[ ! -d "$ANDROID_SDK_ROOT" ]]
+then
+    echo "Error: ANDROID_SDK_ROOT not set or invalid. Aborting..."
+    exit -1
+fi
+
+if [[ ! -d "$ANDROID_NDK_ROOT" ]]
+then
+    echo "Error: ANDROID_NDK_ROOT not set or invalid. Aborting..."
     exit -1
 fi
 
 platform="$1"
 shift 1
-args=(--full)
+args=()
 
 rm -rf build.log
 case "${platform}" in
@@ -22,10 +39,17 @@ case "${platform}" in
     iOS|ios)
         ./ios.sh "${args[@]}"
         ;;
-    clean)
+    --full)
+        args+=(--full)
+        ;;
+    --clean)
         cd tools
         ./clean.sh
         cd ..
+        ;;
+    --help)
+        show_help
+        exit 0
         ;;
     *)
         echo "Error: platform unknown. Aborting..."
